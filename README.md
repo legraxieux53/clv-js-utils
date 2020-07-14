7451c7a6bd9b540c0f662a224fc3b7f1783af912
nehemie__konan <nehemiekonan535@gmail.com>
2020-07-14 23:52:36 +0000

    array utils

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # clv-js-utils
+
+## ClvArrayUtils
+This is an array utils collection for help development
+
+### removeFromArrayByValue(arr: Array<any>, key: string, value: any)
+Remove item in array that has specified value. Key may be a path.
---
+++ src/array-utils.ts
@@ -0,0 +1,20 @@
+import objectPath from 'object-path';
+
+/**
+ * This is an array utils collection for help development
+ */
+export class ClvArrayUtils {
+
+    /**
+     * Remove item in array that has specified value. Key may be a path.
+     */
+    static removeFromArrayByValue(arr: Array<any>, key: string, value: any) {
+        if (!arr) throw new Error('no array');
+        if (!key) throw new Error('no key');
+        if (!value) throw new Error('no value');
+        arr.filter(item => {
+            const data = objectPath.get(item, key);
+            return data === value;
+        });
+    }
+}
